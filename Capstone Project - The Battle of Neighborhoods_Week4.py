8dec1dad16aab73e990c877ba44ca8b48dc94371
bharatwin7 <46638925+bharatwin7@users.noreply.github.com>
2019-01-20 19:45:33 -0500

    Add files via upload

---
+++ Capstone Project - The Battle of Neighborhoods_Week4.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# # Capstone Project - The Battle of Neighborhoods
+# 
+
+# ### Project Description
+
+# A coffee house may be one of the more simple businesses to start among businesses in the food and beverage industry. The ability to plan ahead and meet the needs of customers will help create a successful business that will endure time. The location should be enegry efficient. This project recommends location to an entrepreneur to open a coffee shop in New York City using data science. 
+
+# Whenever people want to open a new shop, they explore the place and try to fetch as much information as possible around it. It can be the neighborhood, venues, etc., This is can be termed as request for a search algorithm which usually returns the requested features such as population rate, schools/colleges/offices around, weather conditions, recreational facilities etc.  It would be beneficial to have an application which could make easy by considering a comparative analysis between the neighborhood with provided factors.
+
+# ### Data Section
+
+# New York City Neighborhood Names point file from https://geo.nyu.edu/catalog/nyu_2451_34572. It has a total of 5 boroughs and 306 neighborhoods. 
+# 
+# #### Foursquare API:
+# It has a database of more than 105 million places. This project would use Four-square API as its prime data gathering source.
+
+# #### Python Library Files :
+#     • Pandas - Library for Data Analysis 
+#     • NumPy – Library to handle data in a vectorized manner
+#     • JSON – Library to handle JSON files 
+#     • Folium – Map rendering Library
+#     • Matplotlib – Python Plotting Module 
+#     • Geopy – To retrieve Location Data 
+#     • Requests – Library to handle http requests
+#     • Sklearn – Python machine learning Library 
+
+# #### Folium:
+# Python visualization library would be used to visualize the neighborhoods cluster distribution of Chicago city over an interactive leaflet map.Extensive comparative analysis of two randomly picked neighborhoods world be carried out to derive the desirable insights from the outcomes using python’s scientific libraries Pandas, NumPy and Scikit-learn.
+# 
+# #### Unsupervised machine learning algorithm:
+# K-mean clustering would be applied to form the clusters of different categories of places  in and around the neighborhoods. Each of them would be analyzed individually and comparatively to derive the best location.
